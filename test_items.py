b0e7f391dba69776d4b4bd5d9c45f9d37f0c3dc6
Zmey14 <mishkaneshishka@gmail.com>
2021-07-15 17:37:34 +0300

    working version 1.2

--- test_items.py
+++ test_items.py
@@ -5,7 +5,5 @@ import time
 def test_start_browser(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    # time.sleep(5)
     button = browser.find_element_by_css_selector(".btn-add-to-basket").is_enabled()
     assert button, "отсутствует button"
-    time.sleep(3)
