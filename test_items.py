86819171b3a34394e0f99b94f7a56fe8948cdf4f
Zmey14 <mishkaneshishka@gmail.com>
2021-07-15 18:01:19 +0300

    working version 1.3

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import pytest
 import time
 
 
-def test_start_browser(browser):
+def test_find_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     button = browser.find_element_by_css_selector(".btn-add-to-basket").is_enabled()
