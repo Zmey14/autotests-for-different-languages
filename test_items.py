828e04a5c25528d4b8bc4490cfa4646df402cbcf
Zmey14 <mishkaneshishka@gmail.com>
2021-07-17 12:01:28 +0300

    bug fixing

--- test_items.py
+++ test_items.py
@@ -1,9 +1,8 @@
-import pytest
 import time
 
 
 def test_find_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    button = browser.find_element_by_css_selector(".btn-add-to-basket").is_enabled()
-    assert button, "отсутствует button"
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, "отсутствует Button"
